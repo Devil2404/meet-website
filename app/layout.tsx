716314cff483e9a4bf937c2fd73441ca5e0870f4
Preet Shah <shahpreet2404@gmail.com>
2026-06-24 20:12:50 +0530

    Clarity added

--- app/layout.tsx
+++ app/layout.tsx
@@ -92,6 +92,18 @@ export default function RootLayout({
     <html lang="en" className="dark">
       <head>
         <script
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(c,l,a,r,i,t,y){
+                  c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "xc36jen23j");
+            `,
+          }}
+        />
+        <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(organizationSchema) }}
         />
